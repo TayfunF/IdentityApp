2df4da5f0206001d09e89f93bd4d8c3dd30c9036
TayfunF <firtinatayfun08@gmail.com>
2022-03-05 10:17:28 +0300

    (Ders 9) # AspNetUsers Tablosundaki üye bilgilerinin gösterilmesi

---
+++ IdentityApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace IdentityApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> _userManager { get; }
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var um = _userManager.Users.ToList();
+            return View(um);
+        }
+    }
+}
