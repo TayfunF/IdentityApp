25ab5a9b1038aa382f9c22ea365525ab563fe02c
TayfunF <firtinatayfun08@gmail.com>
2022-03-05 12:32:29 +0300

    (Ders 10) # Üye ol(Register) sayfasının tasarlanması/kodlanması

--- IdentityApp/Controllers/HomeController.cs
+++ IdentityApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using IdentityApp.Models;
+using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,9 +15,12 @@ namespace IdentityApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public UserManager<AppUser> userManager { get; }
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
@@ -32,6 +37,52 @@ namespace IdentityApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //--------------------------------------------------------------------
+        //ÜYE OL SAYFASI GET-POST METODUM
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserVM userVM)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser();
+                appUser.UserName = userVM.UserName;
+                appUser.Email = userVM.Email;
+                appUser.PhoneNumber = userVM.PhoneNumber;
+                //Şifreyi string olarak tutuyorum. Bunu Hash lemem lazım. O yüzden direk appUser ile almadım.
+                IdentityResult result = await userManager.CreateAsync(appUser, userVM.Password);
+                //Başarıyla kayıt olduysa Login ekranına gönder
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+                //Eğer kullanıcının girdiği değerlerde bir hata varsa bu hatayı göster
+                else
+                {
+                    //Türkçeleştirmesi CustomValidation klasörü ile yapılacak
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+            return View(userVM);
+        }
+
+        //--------------------------------------------------------------------
+        //GİRİŞ YAP SAYFASI GET-POST METODUM
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
         }
     }
 }
---
+++ IdentityApp/ViewModels/UserVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels
+{
+    public class UserVM
+    {
+        [Display(Name = "Kullanıcı Adı :")]
+        [Required(ErrorMessage = "Kullanıcı adı girmek zorunludur !")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "Telefon Numarası :")]
+        public string PhoneNumber { get; set; }
+
+
+        [Display(Name = "E-Posta Adresi :")]
+        [Required(ErrorMessage = "E-posta adresi girmek zorunludur !")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-posta adresi doğru formatta yazılmadı !")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Şifre :")]
+        [Required(ErrorMessage = "Şifre girmek zorunludur !")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifre en az 4 karakter olmalıdır !")]
+        public string Password { get; set; }
+    }
+}
