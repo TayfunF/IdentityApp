771b4d50032033c9dfd6486f49f84791af9556a6
TayfunF <firtinatayfun08@gmail.com>
2022-03-06 18:45:35 +0300

    (Ders 25) # Üye anasayfanın tasarlanması ve Startup Değişiklikler

--- IdentityApp/Startup.cs
+++ IdentityApp/Startup.cs
@@ -28,7 +28,6 @@ namespace IdentityApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
             //---------------------------------------------------------------------------------------------------
             //BURANIN ARASINA EKLEME YAPIYORUM
             //DB Service
@@ -36,87 +35,68 @@ namespace IdentityApp
             //Identity Service
             services.AddIdentity<AppUser, AppRole>(options =>
             {
+                options.User.RequireUniqueEmail = true; //E-posta adresi uniq olmalý dedim.
+                //https://docs.microsoft.com/tr-tr/dotnet/api/microsoft.aspnetcore.identity.useroptions.allowedusernamecharacters?view=aspnetcore-6.0
+                //UserName sadece þu karakterlerden oluþabilir dedim.
+                options.User.AllowedUserNameCharacters = "abcçdefgðhýijklmnoçpqrsþtuüvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+
                 options.Password.RequiredLength = 4; //Sifre en az 4 karakter olabilir.
                 options.Password.RequireNonAlphanumeric = false; //Sifrede * . ? gibi seyler giremesin dedim.
                 options.Password.RequireLowercase = false; //Sifrede Kucuk karakter girme zorunlulugunu kaldirdim.
                 options.Password.RequireUppercase = false; //Sifrede Buyuk karakter girme zorunlulugunu kaldirdim.
                 options.Password.RequireDigit = false; //0 dan 9 a kadar sifre giremesin dedim.
-                options.User.RequireUniqueEmail = true; //E-posta adresi uniq olmalý dedim.
-                //https://docs.microsoft.com/tr-tr/dotnet/api/microsoft.aspnetcore.identity.useroptions.allowedusernamecharacters?view=aspnetcore-6.0
-                //UserName sadece þu karakterlerden oluþabilir dedim.
-                options.User.AllowedUserNameCharacters = "abcçdefgðhýijklmnoöpqrsþtuüvwxyzABCÇDEFGÐHIÝJKLMNOÖPQRSÞTUÜVWXYZ0123456789-._";
             }).AddPasswordValidator<CustomPasswordValidator>()
             .AddUserValidator<CustomUserValidator>()
             .AddErrorDescriber<CustomIdentityErorDescriber>()
             .AddEntityFrameworkStores<AppIdentityDbContext>()
             .AddDefaultTokenProviders();
+
             //Cookiebuilder Service
-            //services.ConfigureApplicationCookie(options =>
-            //{
-            //    options.Cookie = new CookieBuilder()
-            //    {
-            //        Name = "MyBlog", //Cookie Adi
-            //        HttpOnly = false, //Http olursa kabul etme
-            //        SameSite = SameSiteMode.Lax,
-            //        SecurePolicy = CookieSecurePolicy.SameAsRequest //Browsera istek Http ise Http ile al , Https ise Https ile al.
-            //    };
-            //    options.LoginPath = new PathString("/Home/Login"); //Kullanici uye olmadan üyelerin erisebildigi yere tiklarsa Login'e yonlendir.
-            //    //options.LogoutPath = new PathString("/Home/Logout"); //Cikis Yap.
-            //    options.SlidingExpiration = true; //Kullanici 3.5 gün sonra yani 4.gun sonra siteme istek yaparsa 7 gun daha oturumunu sakla.
-            //    options.ExpireTimeSpan = TimeSpan.FromDays(7);
-            //    options.AccessDeniedPath = new PathString("/Home/AccessDenied"); //Erisim Reddedildi Sayfasi
-            //    //Mvc Service
-            //    services.AddMvc();
-            //});
-            CookieBuilder cookieBuilder = new CookieBuilder();
-            cookieBuilder.Name = "MyBlog";
-            cookieBuilder.HttpOnly = false;
-            cookieBuilder.SameSite = SameSiteMode.Lax;
-            cookieBuilder.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-            services.ConfigureApplicationCookie(opts =>
+            services.ConfigureApplicationCookie(options =>
             {
-                opts.LoginPath = new PathString("/Home/Login");
-                opts.LogoutPath = new PathString("/Member/LogOut");
-                opts.Cookie = cookieBuilder;
-                opts.SlidingExpiration = true;
-                opts.ExpireTimeSpan = System.TimeSpan.FromDays(60);
-                opts.AccessDeniedPath = new PathString("/Member/AccessDenied");
+                options.Cookie = new CookieBuilder()
+                {
+                    Name = "MyBlog", //Cookie Adi
+                    HttpOnly = false, //Http olursa kabul etme
+                    SameSite = SameSiteMode.Lax,
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest //Browsera istek Http ise Http ile al , Https ise Https ile al.
+                };
+                options.LoginPath = new PathString("/Home/Login"); //Kullanici uye olmadan üyelerin erisebildigi yere tiklarsa Login'e yonlendir.
+                options.LogoutPath = new PathString("/Member/Logout"); //Cikis Yap.
+                options.SlidingExpiration = true; //Kullanici 30 gun sonra siteme istek yaparsa 60 gun daha oturumunu sakla.
+                options.ExpireTimeSpan = System.TimeSpan.FromDays(60);
+                options.AccessDeniedPath = new PathString("/Member/AccessDenied"); //Erisim Reddedildi Sayfasi
             });
             //BURANIN ARASINA EKLEME YAPIYORUM
             //---------------------------------------------------------------------------------------------------
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
+            
             app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            //---------------------------------------------------------------------------------------------------
-            //BURANIN ARASINA EKLEME YAPIYORUM
-            app.UseStatusCodePages();
-            app.UseAuthentication();
-            //BURANIN ARASINA EKLEME YAPIYORUM
-            //---------------------------------------------------------------------------------------------------
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            //app.UseExceptionHandler("/Home/Error");
+
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //app.UseHsts();
+
+            //app.UseHttpsRedirection();
         }
     }
 }
