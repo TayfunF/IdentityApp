a843694a8db46fa50994e523a826526e863ce49b
TayfunF <firtinatayfun08@gmail.com>
2022-03-06 19:34:10 +0300

    (Ders 25) # Üye anasayfanın tasarlanması/kodlanması 2

--- IdentityApp/Controllers/MemberController.cs
+++ IdentityApp/Controllers/MemberController.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Mapster;
+using IdentityApp.ViewModels;
 
 namespace IdentityApp.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            AppUser appUser = userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserVM userVM = appUser.Adapt<UserVM>(); //Mapster Kutuphane Kullanimi
+            return View(userVM);
         }
     }
 }
