45c5e44ba914f008e62c8dd6f604442dc6cf2284
TayfunF <firtinatayfun08@gmail.com>
2022-03-08 22:02:44 +0300

    (Ders 35) # Rol oluşturma

--- IdentityApp/Controllers/AdminController.cs
+++ IdentityApp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityApp.Models;
+using IdentityApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
@@ -8,7 +9,7 @@ namespace IdentityApp.Controllers
     public class AdminController : BaseController
     {
 
-        public AdminController(UserManager<AppUser> userManager,RoleManager<AppRole> roleManager):base(userManager,null,roleManager)
+        public AdminController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager) : base(userManager, null, roleManager)
         {
         }
         public IActionResult Index()
@@ -16,9 +17,43 @@ namespace IdentityApp.Controllers
             return View();
         }
 
+        //--------------------------------------------------------------------
+        //UYELER SAYFASI GET METODUM
         public IActionResult Users()
         {
             return View(userManager.Users.ToList());
+        }
+
+        //--------------------------------------------------------------------
+        //ROLLER SAYFASI GET METODUM
+        public IActionResult Roles()
+        {
+            return View(roleManager.Roles.ToList());
+        }
+
+        //--------------------------------------------------------------------
+        //UYE ROLU EKLEME SAYFASI GET-POST METODUM
+        [HttpGet]
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RoleCreate(RoleVM roleVM)
+        {
+            AppRole role = new AppRole();
+            role.Name = roleVM.Name;
+            IdentityResult result = roleManager.CreateAsync(role).Result;
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Roles", "Admin");
+            }
+            else
+            {
+                AddModelError(result);
+            }
+            return View(roleVM);
         }
     }
 }
---
+++ IdentityApp/ViewModels/RoleVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels
+{
+    public class RoleVM
+    {
+        [Display(Name="Rol İsmi :")]
+        [Required(ErrorMessage ="Role ismi boş bırakılamaz")]
+        public string Name { get; set; }
+        public string Id { get; set; } //Rol guncellerken kullanicam
+    }
+}
